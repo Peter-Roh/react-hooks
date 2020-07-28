c33dceda2918305d79c9afa35d1c5d56fb22fbd8
Peter-Roh <rmc2@naver.com>
2020-07-28 15:13:31 +0900

    react-hooks

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,16 @@
+export const useConfirm = (message="", onConfirm, rejection) => {
+    if(!onConfirm || typeof onConfirm !== "function") {
+        return;
+    }
+    if(rejection && typeof rejection !== "function") {
+        return;
+    }
+    const confirmAction = () => {
+        if(window.confirm(message)) {
+            onConfirm();
+        } else {
+            rejection();
+        }
+    };
+    return confirmAction;
+};
